788378babe98257b1f7882ae33c4dfe867559603
Aaron Lee <leeaaron326@gmail.com>
2019-02-16 13:01:19 -0500

    Adding test

---
+++ tests/test_data.js
@@ -0,0 +1,30 @@
+let primary = {};
+
+primary.personName = 'Aaron Lee';
+primary.age = 26;
+primary.hobbies = ['Powerlifting', 'Coding', 'Reading'];
+
+let mother = {};
+
+mother.personName = 'Cindy Lee';
+mother.age = 57;
+mother.hobbies = ['Gardening', 'Cleaning'];
+
+let father = {};
+
+father.personName = 'Alex Lee';
+father.age = 62;
+father.hobbies = ['Fishing', 'Hunting']
+
+let family = {}
+
+family.primary = primary;
+family.mother = mother;
+family.father = father;
+family.size = 3;
+family.livingSpace = 'House'
+
+console.log(family);
+
+
+
